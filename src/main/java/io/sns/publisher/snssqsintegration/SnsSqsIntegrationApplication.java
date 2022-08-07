f07576a5501e46da0e4243c540ed4978e4b40015
Hardik Gunjal <hardikgunjal2928@gmail.com>
2022-08-07 15:36:19 -0400

    inital commit

---
+++ src/main/java/io/sns/publisher/snssqsintegration/SnsSqsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package io.sns.publisher.snssqsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsSqsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsSqsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sns/publisher/snssqsintegration/SnsSqsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sns.publisher.snssqsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsSqsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
